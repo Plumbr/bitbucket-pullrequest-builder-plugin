056f779041b0f1864df47a93a07b652150f37baf
Nikita Salnikov-Tarnovski <nikem@plumbr.eu>
2019-03-19 16:52:51 +0200

    Better way to detect if there is changes worth building

--- src/main/java/bitbucketpullrequestbuilder/bitbucketpullrequestbuilder/BitbucketPullRequestsBuilder.java
+++ src/main/java/bitbucketpullrequestbuilder/bitbucketpullrequestbuilder/BitbucketPullRequestsBuilder.java
@@ -1,11 +1,16 @@
 package bitbucketpullrequestbuilder.bitbucketpullrequestbuilder;
 
 import bitbucketpullrequestbuilder.bitbucketpullrequestbuilder.bitbucket.AbstractPullrequest;
+import hudson.FilePath;
+import hudson.Launcher;
 import hudson.model.Job;
 import hudson.model.TaskListener;
 import hudson.plugins.mercurial.MercurialSCM;
 import hudson.scm.PollingResult;
+import hudson.scm.SCMRevisionState;
 import hudson.util.LogTaskListener;
+import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -20,22 +25,22 @@ import jenkins.triggers.SCMTriggerItem;
 import org.apache.commons.codec.binary.Hex;
 
 public class BitbucketPullRequestsBuilder {
-    private static final Logger logger = Logger.getLogger(BitbucketBuildTrigger.class.getName());
-    private Job<?, ?> job;
-    private BitbucketBuildTrigger trigger;
-    private BitbucketRepository repository;
-    private BitbucketBuilds builds;
+  private static final Logger logger = Logger.getLogger(BitbucketBuildTrigger.class.getName());
+  private Job<?, ?> job;
+  private BitbucketBuildTrigger trigger;
+  private BitbucketRepository repository;
+  private BitbucketBuilds builds;
 
-    public static BitbucketPullRequestsBuilder getBuilder() {
-        return new BitbucketPullRequestsBuilder();
-    }
+  public static BitbucketPullRequestsBuilder getBuilder() {
+    return new BitbucketPullRequestsBuilder();
+  }
 
-    public void stop() {
-        // TODO?
-    }
+  public void stop() {
+    // TODO?
+  }
 
-    public void run() {
-        this.repository.init();
+  public void run() {
+    this.repository.init();
     LogTaskListener taskListener = new LogTaskListener(logger, Level.INFO);
 
     List<AbstractPullrequest> targetPullRequests = new ArrayList<>();
@@ -45,8 +50,8 @@ public class BitbucketPullRequestsBuilder {
       }
     }
 
-        this.repository.addFutureBuildTasks(targetPullRequests);
-    }
+    this.repository.addFutureBuildTasks(targetPullRequests);
+  }
 
   /*
     Checks if given pull request changed files that this job is interested in.
@@ -60,21 +65,23 @@ public class BitbucketPullRequestsBuilder {
       MercurialSCM scm = (MercurialSCM) scmTriggerItem.getSCMs().iterator().next();
       String initialRevision = scm.getRevision();
 
+      Launcher.LocalLauncher launcher = new Launcher.LocalLauncher(taskListener);
+      File repository = new File(System.getProperty("java.io.tmpdir"), job.getName());
+
       String destinationBranchName = pullRequest.getDestination().getBranch().getName();
-      logger.log(Level.INFO, "Resetting poll baseline to target branch: {0}", destinationBranchName);
-      scm.setRevision(destinationBranchName);
-      scmTriggerItem.poll(taskListener);
+      logger.log(Level.INFO, "Getting target branch state: {0}", destinationBranchName);
+      SCMRevisionState destinationBranchState = getDestinationBranchState(launcher, taskListener, repository, destinationBranchName, scm);
 
       String sourceBranchName = pullRequest.getSource().getBranch().getName();
       logger.log(Level.INFO, "Now to actual poll for changes in branch {0}", sourceBranchName);
       scm.setRevision(sourceBranchName);
-      PollingResult pollingResult = scmTriggerItem.poll(taskListener);
+      PollingResult pollingResult = scm.compareRemoteRevisionWith(job, launcher, new FilePath(repository), taskListener, destinationBranchState);
 
       logger.log(Level.INFO, "Branch changes are {0}", pollingResult.change);
       scm.setRevision(initialRevision);
 
       return pollingResult.hasChanges();
-    } catch (Error | RuntimeException e) {
+    } catch (Exception e) {
       e.printStackTrace(taskListener.error("Failed to record SCM polling"));
       logger.log(Level.SEVERE, "Failed to record SCM polling", e);
       //If cannot determine changes, build anyway
@@ -82,49 +89,56 @@ public class BitbucketPullRequestsBuilder {
     }
   }
 
-    public BitbucketPullRequestsBuilder setupBuilder() {
-        if (this.job == null || this.trigger == null) {
-            throw new IllegalStateException();
-        }
-        this.repository = new BitbucketRepository(this.trigger.getProjectPath(), this);
-        this.repository.init();
-        this.builds = new BitbucketBuilds(this.trigger, this.repository);
-        return this;
-    }
+  private SCMRevisionState getDestinationBranchState(Launcher launcher, TaskListener listener, File repository, String branchName, MercurialSCM scm) throws IOException, InterruptedException {
+    scm.setRevision(branchName);
+    scm.checkout(job.getLastBuild(), launcher, new FilePath(repository), listener, null, null);
+    return scm.calcRevisionsFromBuild(job.getLastBuild(), new FilePath(repository), launcher, listener);
+  }
 
-    public void setJob(Job<?, ?> job) {
-        this.job = job;
-    }
 
-    public void setTrigger(BitbucketBuildTrigger trigger) {
-        this.trigger = trigger;
+  public BitbucketPullRequestsBuilder setupBuilder() {
+    if (this.job == null || this.trigger == null) {
+      throw new IllegalStateException();
     }
+    this.repository = new BitbucketRepository(this.trigger.getProjectPath(), this);
+    this.repository.init();
+    this.builds = new BitbucketBuilds(this.trigger, this.repository);
+    return this;
+  }
 
-    public Job<?, ?> getJob() {
-        return this.job;
-    }
+  public void setJob(Job<?, ?> job) {
+    this.job = job;
+  }
 
-    /**
-     * Return MD5 hashed full project name or full project name, if MD5 hash provider inaccessible
+  public void setTrigger(BitbucketBuildTrigger trigger) {
+    this.trigger = trigger;
+  }
+
+  public Job<?, ?> getJob() {
+    return this.job;
+  }
+
+  /**
+   * Return MD5 hashed full project name or full project name, if MD5 hash provider inaccessible
    *
-     * @return unique project id
-     */
-    public String getProjectId() {
-      try {
-        final MessageDigest MD5 = MessageDigest.getInstance("MD5");
+   * @return unique project id
+   */
+  public String getProjectId() {
+    try {
+      final MessageDigest MD5 = MessageDigest.getInstance("MD5");
       return new String(Hex.encodeHex(MD5.digest(this.job.getFullName().getBytes(StandardCharsets.UTF_8))));
-      } catch (NoSuchAlgorithmException exc) {
-        logger.log(Level.WARNING, "Failed to produce hash", exc);
-      }
-      return this.job.getFullName();
-
+    } catch (NoSuchAlgorithmException exc) {
+      logger.log(Level.WARNING, "Failed to produce hash", exc);
     }
+    return this.job.getFullName();
 
-    public BitbucketBuildTrigger getTrigger() {
-        return this.trigger;
-    }
+  }
 
-    public BitbucketBuilds getBuilds() {
-        return this.builds;
-    }
+  public BitbucketBuildTrigger getTrigger() {
+    return this.trigger;
+  }
+
+  public BitbucketBuilds getBuilds() {
+    return this.builds;
+  }
 }
